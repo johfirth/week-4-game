425b39db1b18447ce5071998ca7f4e5bda3b297c
John Firth <johfirth@gmail.com>
2018-03-11 13:45:42 -0400

    first little pieces

---
+++ game.js
@@ -0,0 +1,16 @@
+var randomNumber = Math.floor((Math.random() * 100) + 1);
+var diamond = 1;
+var circle = 5;
+var square = 8;
+var triangle = 4;
+
+$("#mainNumber").text(randomNumber);
+
+$("#diamond").on('click', function(){
+    
+
+})
+
+$("#circle").on('click', function(){
+
+})
