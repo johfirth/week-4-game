af216965680c3adaba028b0aa975070b69dbbdb2
John Firth <johfirth@gmail.com>
2018-03-11 14:53:53 -0400

    click functions working, numbers adding

--- game.js
+++ game.js
@@ -1,16 +1,30 @@
 var randomNumber = Math.floor((Math.random() * 100) + 1);
-var diamond = 1;
-var circle = 5;
-var square = 8;
-var triangle = 4;
+var userGuess = 0;
+var diamond = Math.floor((Math.random() * 5) + 1);
+var circle = Math.floor((Math.random() * 10) + 1);
+var square = Math.floor((Math.random() * 15) + 1);
+var green = Math.floor((Math.random() * 10) + 1);
 
 $("#mainNumber").text(randomNumber);
+$('#currentGuess').text(userGuess);
 
-$("#diamond").on('click', function(){
-    
-
+$("#diamond").on('click', function(){  
+    userGuess = userGuess + diamond;
+    $('#currentGuess').text(userGuess);
 })
 
 $("#circle").on('click', function(){
-
+    userGuess = userGuess + circle;
+    $('#currentGuess').text(userGuess);
 })
+
+$('#square').on('click', function(){
+    userGuess = userGuess + square;
+    $('#currentGuess').text(userGuess);
+})
+
+$('#green').on('click', function (){
+    userGuess = userGuess + green;
+    $('#currentGuess').text(userGuess);
+})
+
